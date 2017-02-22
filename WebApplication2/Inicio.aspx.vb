d61f47b9eb2fa04b1465ec656f1094a8d16c8f25
EduardoIker <eduardoikersw@gmail.com>
2017-02-22 19:28:12 +0100

    Add files via upload

---
+++ WebApplication2/Inicio.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports AccesoDatos.accesoDatosSQL
+
+Public Class WebForm1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        conectar()
+    End Sub
+
+    Protected Sub Page_Unload(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Unload
+        cerrarConexion()
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim resultado As Boolean
+        resultado = login(TextBox1.Text, TextBox2.Text)
+        If (resultado) Then
+            Session.Contents("correo") = TextBox1.Text
+            Response.Redirect("pagina.aspx")
+        ElseIf (Not resultado) Then
+            Label2.Text = "Datos de acceso incorrectos. Inténtalo de nuevo."
+        Else
+            Label2.Text = "Ha ocurrido un problema inesperado. Inténtalo de nuevo."
+        End If
+    End Sub
+
+
+End Class
---
+++ WebApplication2/obj/Release/Package/PackageTmp/pagina.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="pagina.aspx.vb" Inherits="Laboratorio2.WebForm5" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Font-Size="X-Large" ForeColor="Lime" Text="Login Correcto"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
